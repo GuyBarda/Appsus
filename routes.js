3b278b9b32234b8cb8d547bf1002725edd080e40
Guy Barda <guy.barda.1234@gmail.com>
2022-11-09 15:08:57 +0200

    merge

--- routes.js
+++ routes.js
@@ -1,10 +1,6 @@
 import homePage from "./views/app-home.cmp.js";
 import aboutPage from "./views/app-about.cmp.js";
-<<<<<<< HEAD
 import emailApp from "./apps/mail/pages/email-app.cmp.js";
-=======
-import emailApp from "./views/email-app.cmp.js";
->>>>>>> cc4b6ce9ca295ce7a674d281212f44e1017cf7b1
 import keepApp from "./apps/keep/pages/keep-app.cmp.js";
 
 const { createRouter, createWebHashHistory } = VueRouter;
