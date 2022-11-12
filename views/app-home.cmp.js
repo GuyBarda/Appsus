4fbd0a788a7b672b9cc466e6e2ce97c4c2c82af6
Omer Mazig <omazig93@gmail.com>
2022-11-13 01:26:31 +0200

    final2

--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -4,13 +4,13 @@ export default {
             <section class="app-home-container main-layout">
                 <div class="content-container">
                     <h1>Welcome to the future!</h1>
-                    <h3>Store you info like in the old days but its nowdays!</h3>
+                    <!-- <h3>Store you info like in the old days but its nowdays!</h3>
                     <h3>Send & recive mail like a good boy!</h3>
-                    <h3>Here, now and tomorrow.</h3>
+                    <h3>Here, now and tomorrow.</h3> -->
                 </div>
 
                 <div class="main-img-container">
-                    <img src="/img/home.png" alt="map" />
+                    <img src="./img/home.png" alt="map" />
                 </div>
             </section>
     `,
